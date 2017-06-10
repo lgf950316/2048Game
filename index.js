500c3408043fa27d5eb41b23af2a11c27ab2dd83
lgf950316 <597652117@qq.com>
2017-06-10 14:54:09 +0800

    random number and graphics

--- index.js
+++ index.js
@@ -3,10 +3,11 @@
  */
 var app = new PIXI.Application(window.innerWidth, window.innerHeight, {backgroundColor: 0x54FF9F});
 document.body.appendChild(app.view);
-var basicText = new PIXI.Text('2048', {fontSize: 200});
 
-basicText.x = app.renderer.width/3.6;
-basicText.y = app.renderer.height/9;
+var basicText = new PIXI.Text('2048', {fontSize: 200});
+basicText.anchor.set(0.5);
+basicText.x = app.renderer.width / 2;
+basicText.y = app.renderer.height / 6;
 app.stage.addChild(basicText);
 
 var grid = [];
@@ -18,9 +19,27 @@ for (var i = 0; i < 4; i++) {
 for (var i = 0; i < 4; i++) {
     for (var j = 0; j < 4; j++) {
         var graphics = new PIXI.Graphics();
-        //graphics.lineStyle(2, 0x0000FF, 1);
         graphics.beginFill(0xFFA54F, 1);
-        graphics.drawRect(app.renderer.width / 8.5  + 190 * j, app.renderer.height / 8 * 2.5 + 190 * i, 185, 185);
+        graphics.drawRect(app.renderer.width / 8.5 + 190 * j, app.renderer.height / 8 * 2.5 + 190 * i, 185, 185);
         app.stage.addChild(graphics);
     }
 }
+
+//随机生成数字
+function randomNumber() {
+    return Math.floor(Math.random() * 4);
+}
+
+var x = randomNumber();
+var y = randomNumber();
+
+var graphics = new PIXI.Graphics();
+graphics.beginFill(0xFF0000, 1);
+graphics.drawRect(x * 190 + app.renderer.width / 8.5, y * 190 + app.renderer.height / 8 * 2.5, 185, 185);
+app.stage.addChild(graphics);
+
+var number = new PIXI.Text('2', {fontSize: 150});
+number.anchor.set(0.5);
+number.x = 185 / 2 + app.renderer.width / 8.5 + x * 190;
+number.y = 185 / 2 + app.renderer.height / 8 * 2.5 + y * 190;
+app.stage.addChild(number);
