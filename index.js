e478514cad7728e88d434ed2204ae8977fdb2f49
lgf950316 <597652117@qq.com>
2017-06-10 17:13:59 +0800

    toRight and merge

--- index.js
+++ index.js
@@ -12,20 +12,17 @@ app.stage.addChild(basicText);
 
 var grid = [];
 for (var i = 0; i < 4; i++) {
-    grid[i] = [0, 0, 0, 0]
+    grid[i] = [0, 0,0, 0]
 }
 
-//画网格
-for (var i = 0; i < 4; i++) {
-    for (var j = 0; j < 4; j++) {
-        var graphics = new PIXI.Graphics();
-        graphics.beginFill(0xFFA54F, 1);
-        graphics.drawRect(app.renderer.width / 8.5 + 190 * j, app.renderer.height / 8 * 2.5 + 190 * i, 185, 185);
-        app.stage.addChild(graphics);
-
-        drawcell(i, j);
+function flushUI() {
+    for (var i = 0; i < 4; i++) {
+        for (var j = 0; j < 4; j++) {
+            drawcell(i, j);
+        }
     }
 }
+
 
 //随机生成数字
 function randomNumber() {
@@ -33,16 +30,13 @@ function randomNumber() {
 }
 
 function drawcell(x, y) {
-    var color = 0xFFA54F;
-    if (grid[x][y] == 2) {
-        color = 0xff0000;
-    }
+
     var graphics = new PIXI.Graphics();
-    graphics.beginFill(color, 1);
+    graphics.beginFill(getColorByNumber(grid[x][y]), 1);
     graphics.drawRect(y * 190 + app.renderer.width / 8.5, x * 190 + app.renderer.height / 8 * 2.5, 185, 185);
     app.stage.addChild(graphics);
 
-    if (grid[x][y] == 2) {
+    if (grid[x][y] !== 0) {
         var number = new PIXI.Text(grid[x][y], {fontSize: 150});
         number.anchor.set(0.5);
         number.x = 185 / 2 + app.renderer.width / 8.5 + y * 190;
@@ -51,14 +45,54 @@ function drawcell(x, y) {
     }
 }
 
-var x = randomNumber();
-var y = randomNumber();
-grid[x][y] = 2;
+function getColorByNumber(number){
+    var colorValue={
+        0:0xFFA54F,
+        2:0x00ffff,
+        4:0x0000ff
+    }
+    return colorValue[number];
+}
 
-drawcell(x, y);
+var rwoIndex = randomNumber();
+var columnIndex = randomNumber();
+grid[rwoIndex][columnIndex] = 2;
+flushUI();
+
 document.addEventListener("keydown", function (event) {
     if (event.keyCode == 39) {
-        console.log(event);
+        moveCellToRight();
+        flushUI();
     }
 
 })
+
+function moveCellToRight() {
+    for (var rowIndex = 0; rowIndex < 4; rowIndex++) {
+        for (var columnIndex = 2; columnIndex >= 0; columnIndex--) {
+            if (grid[rowIndex][columnIndex] === 0) continue;
+
+            var theEmptyCellIndex = findTheFirstRightCell(rowIndex, columnIndex);
+            if (theEmptyCellIndex !== -1) {
+                grid[rowIndex][theEmptyCellIndex] = grid[rowIndex][columnIndex];
+                grid[rowIndex][columnIndex] = 0;
+
+                if (grid[rowIndex][theEmptyCellIndex] === grid[rowIndex][theEmptyCellIndex + 1]) {
+                    grid[rowIndex][theEmptyCellIndex+ 1] += grid[rowIndex][theEmptyCellIndex];
+                    grid[rowIndex][theEmptyCellIndex] = 0;
+                }
+            }
+
+        }
+    }
+}
+
+function findTheFirstRightCell(rowIndex, columnIndex) {
+    for (var i = 3; i > columnIndex; i--) {
+        if (grid[rowIndex][i] === 0) {
+            return i;
+        }
+    }
+
+    return -1;
+}
