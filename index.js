522866c0e998c5743ca430c4de06b59dc9778a96
lgf950316 <597652117@qq.com>
2017-06-10 13:54:24 +0800

    grid change

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 /**
  * Created by O'Sullivan on 2017/6/10/010.
  */
-var app = new PIXI.Application(window.innerWidth, window.innerHeight, {backgroundColor: 0x1099bb});
+var app = new PIXI.Application(window.innerWidth, window.innerHeight, {backgroundColor: 0x54FF9F});
 document.body.appendChild(app.view);
 var basicText = new PIXI.Text('2048', {fontSize: 200});
 
-basicText.x = app.renderer.width/3.5;
-basicText.y = app.renderer.height/8;
+basicText.x = app.renderer.width/3.6;
+basicText.y = app.renderer.height/9;
 app.stage.addChild(basicText);
 
 var grid = [];
@@ -18,9 +18,9 @@ for (var i = 0; i < 4; i++) {
 for (var i = 0; i < 4; i++) {
     for (var j = 0; j < 4; j++) {
         var graphics = new PIXI.Graphics();
-        graphics.lineStyle(2, 0x0000FF, 1);
-        graphics.beginFill(0xFF700B, 1);
-        graphics.drawRect(app.renderer.width / 5.5  + 155 * j, app.renderer.height / 8 * 2.5 + 155 * i, 150, 150);
+        //graphics.lineStyle(2, 0x0000FF, 1);
+        graphics.beginFill(0xFFA54F, 1);
+        graphics.drawRect(app.renderer.width / 8.5  + 190 * j, app.renderer.height / 8 * 2.5 + 190 * i, 185, 185);
         app.stage.addChild(graphics);
     }
 }
