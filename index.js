c80407ba6e5c795dd9c99755463344d6006274e5
lgf950316 <597652117@qq.com>
2017-06-10 16:05:39 +0800

    drawcell & eventlisten

--- index.js
+++ index.js
@@ -22,6 +22,8 @@ for (var i = 0; i < 4; i++) {
         graphics.beginFill(0xFFA54F, 1);
         graphics.drawRect(app.renderer.width / 8.5 + 190 * j, app.renderer.height / 8 * 2.5 + 190 * i, 185, 185);
         app.stage.addChild(graphics);
+
+        drawcell(i, j);
     }
 }
 
@@ -30,16 +32,33 @@ function randomNumber() {
     return Math.floor(Math.random() * 4);
 }
 
+function drawcell(x, y) {
+    var color = 0xFFA54F;
+    if (grid[x][y] == 2) {
+        color = 0xff0000;
+    }
+    var graphics = new PIXI.Graphics();
+    graphics.beginFill(color, 1);
+    graphics.drawRect(y * 190 + app.renderer.width / 8.5, x * 190 + app.renderer.height / 8 * 2.5, 185, 185);
+    app.stage.addChild(graphics);
+
+    if (grid[x][y] == 2) {
+        var number = new PIXI.Text(grid[x][y], {fontSize: 150});
+        number.anchor.set(0.5);
+        number.x = 185 / 2 + app.renderer.width / 8.5 + y * 190;
+        number.y = 185 / 2 + app.renderer.height / 8 * 2.5 + x * 190;
+        app.stage.addChild(number);
+    }
+}
+
 var x = randomNumber();
 var y = randomNumber();
+grid[x][y] = 2;
 
-var graphics = new PIXI.Graphics();
-graphics.beginFill(0xFF0000, 1);
-graphics.drawRect(x * 190 + app.renderer.width / 8.5, y * 190 + app.renderer.height / 8 * 2.5, 185, 185);
-app.stage.addChild(graphics);
+drawcell(x, y);
+document.addEventListener("keydown", function (event) {
+    if (event.keyCode == 39) {
+        console.log(event);
+    }
 
-var number = new PIXI.Text('2', {fontSize: 150});
-number.anchor.set(0.5);
-number.x = 185 / 2 + app.renderer.width / 8.5 + x * 190;
-number.y = 185 / 2 + app.renderer.height / 8 * 2.5 + y * 190;
-app.stage.addChild(number);
+})
